71734e1616f591c2b3c6c78b86aada304816b00f
“euroledger” <geominat@gmail.com>
2023-12-22 20:58:13 +0200

    Add NavBar
    Signed-off-by: “euroledger” <“geominat@gmail.com”>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { React, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import Board from "./components/Board";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Form from "react-bootstrap/Form";
 import {
   TransformWrapper,
   TransformComponent,
@@ -26,38 +32,80 @@ function App() {
   const Controls = () => {
     const { zoomIn, zoomOut, resetTransform } = useControls();
     return (
-      <div class="container">
-        <h1 className="display-2 text-center">Fury at Midway PC Version</h1>
-        <div class="mt-5 mb-5 panel panel-primary text-center">
-          <div id="button-container" class="btn-group">
-            <button class="btn btn-primary" onClick={() => zoomIn()}>
-              Zoom In
-            </button>
-            <button class="ms-2 btn btn-primary" onClick={() => zoomOut()}>
-              Zoom Out
-            </button>
-            <button
-              class="ms-2 btn btn-primary"
-              onClick={() => resetTransform()}
-            >
-              Reset
-            </button>
-          </div>
-        </div>
-      </div>
+      // <div class="container">
+      //   <h1 className="display-2 text-center">Fury at Midway PC Version</h1>
+      //   <div class="mt-5 mb-5 panel panel-primary text-center">
+      //     <div id="button-container" class="btn-group">
+      //       <button class="btn btn-primary" onClick={() => zoomIn()}>
+      //         Zoom In
+      //       </button>
+      //       <button class="ms-2 btn btn-primary" onClick={() => zoomOut()}>
+      //         Zoom Out
+      //       </button>
+      //       <button
+      //         class="ms-2 btn btn-primary"
+      //         onClick={() => resetTransform()}
+      //       >
+      //         Reset
+      //       </button>
+      //     </div>
+      //   </div>
+      // </div>
+      <Navbar
+        bg="primary"
+        data-bs-theme="dark"
+        sticky="top"
+        className="justify-content-between"
+      >
+        <Container>
+          <Navbar.Brand href="/">Save Game</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">US Hand</Nav.Link>
+              <Nav.Link href="/">Japan Hand</Nav.Link>
+              <Nav.Link href="/about">Roll Dice</Nav.Link>
+            </Nav>
+            <ButtonGroup className="ms-auto" aria-label="Basic example">
+              <Button
+                className="me-1"
+                variant="secondary"
+                onClick={() => zoomIn()}
+              >
+                Zoom In
+              </Button>
+              <Button
+                className="me-1"
+                variant="secondary"
+                onClick={() => zoomOut()}
+              >
+                Zoom Out
+              </Button>
+              <Button
+                className="me-1"
+                variant="secondary"
+                onClick={() => resetTransform()}
+              >
+                Reset
+              </Button>
+            </ButtonGroup>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
   };
   const initialJpAopPosition = { left: 2.7, top: 7 };
 
-  function gameStateHandler () {
-    setGameState(!gameState); 
-  }
-  
-  function handleScaleChange(event) {
-    setScale(event.instance.transformState.scale)
+  function gameStateHandler() {
+    setGameState(!gameState);
   }
 
-  GlobalGameState.stateHandler = gameStateHandler
+  function handleScaleChange(event) {
+    setScale(event.instance.transformState.scale);
+  }
+
+  GlobalGameState.stateHandler = gameStateHandler;
+
   return (
     <>
       <TransformWrapper
@@ -70,10 +118,16 @@ function App() {
         // pinch={{ step: 5 }}
       >
         <Controls />
+
         <div class="d-flex p-2">
           <GameStatePanel gameState={gameState} />
           <TransformComponent>
-            <Board gameStateHandler={gameStateHandler} onDrag={onDrag} onStop={onStop} scale={scale}/>
+            <Board
+              gameStateHandler={gameStateHandler}
+              onDrag={onDrag}
+              onStop={onStop}
+              scale={scale}
+            />
           </TransformComponent>
         </div>
       </TransformWrapper>
